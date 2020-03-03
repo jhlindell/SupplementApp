a09a3be0c1b8e48584224d8dd776877fe3e8d7d5
jon lindell <jon.h.lindell@gmail.com>
2020-03-03 13:44:52 -0700

    can display simple list of supplements from server

--- src/components/Supplements/SupplementMain.js
+++ src/components/Supplements/SupplementMain.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchSupplements } from './actions';
 
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    height: '92vh',
-  },
-  ingredientCard: {
-    display: 'flex',
-    margin: '20px auto auto auto',
-    width: '70%',
-    flexDirection: 'column',
-  },
-  cardHeader: {
-    backgroundColor: 'lightgray',
-  },
-  cardContent: {
-    padding: 20,
-    display: 'flex',
-    flexDirection: 'column',
-  },
+  root: {},
 });
 
 function SupplementMain() {
+  const supplements = useSelector(state => state.supplements);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchSupplements());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <h1>Welcome to Supplements!</h1>
-    </div>
-  );
+
+  function renderSupplements() {
+    if (supplements.length) {
+      return (
+        <div className={classes.root}>
+          {supplements.map(supplement => {
+            return <div key={supplement.name}>{supplement.name}</div>;
+          })}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return <div className={classes.root}>{renderSupplements()}</div>;
 }
 
 export default SupplementMain;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import ingredientsReducer from '../components/Ingredients/reducers/ingredients';
 import ingredientReducer from '../components/Ingredients/reducers/ingredient';
+import supplementsReducer from '../components/Supplements/reducers/supplements';
 
 const reducers = combineReducers({
   ingredients: ingredientsReducer,
   ingredient: ingredientReducer,
+  supplements: supplementsReducer,
 });
 
 export default reducers;
